bb39f7addd0586ad57a70e66fac4a174caf7fdd5
Miki2531 <mikiyasshemelis46@gmail.com>
2023-06-26 11:38:35 +0300

    update

--- script.js
+++ script.js
@@ -99,10 +99,11 @@ class App {
   }
 
   _netWorkout(e) {
-    e.preventDefault();
     const validInputs = (...inputs) =>
       inputs.every(inp => Number.isFinite(inp));
     const allPostive = (...inputs) => inputs.every(inp => inp > 0);
+
+    e.preventDefault();
 
     // Get data from form
     const type = inputType.value;
